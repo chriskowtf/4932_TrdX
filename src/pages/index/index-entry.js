23a479e09cfb705a8685bdc31ca9d8d7f37626fb
Kristian <chriskowtf@gmail.com>
2023-03-11 13:28:17 +0200

    Add animation v1.3 FINAL

--- src/pages/index/index-entry.js
+++ src/pages/index/index-entry.js
@@ -20,23 +20,105 @@ let counterArr = Array.from({ length: 29 }, (_, i) => `#l${i + 1}`);
 let counterArr2 = [
     "#line001",
 ]
+// let counterArr3 = [
+// ]
+// document.querySelectorAll(counterArr3).forEach(function (elem) {
+//     let tempLength = (elem.getTotalLength() + 1).toFixed(0)
+//     gsap.set(elem, { strokeDasharray: tempLength, strokeDashoffset: tempLength })
+// })
+const crosshair = document.querySelector("#arrow");
+gsap.set(crosshair, { xPercent: -0.1, yPercent: 0.8 });
 
+// Встановлюємо початкові координати центру екрану
+let centerX = window.innerWidth / 2;
+let centerY = window.innerHeight / 2;
+
+// Встановлюємо функцію, яка виконується кожен раз, коли курсор рухається над .display-animation
+function onMouseMove(e) {
+    // Перевіряємо, чи наведено курсор на .display-animation
+    const displayAnimation = document.querySelector('.display-animation');
+    if (!displayAnimation.contains(e.target)) {
+        return;
+    }
+
+    // Отримуємо координати курсора
+    const x = e.clientX;
+    const y = e.clientY;
+
+    // Обчислюємо різницю між координатами курсора і центром екрану
+    const diffX = x - centerX;
+    const diffY = y - centerY;
+
+    // Анімація руху прицілу за курсором
+    gsap.to(crosshair, {
+        duration: 0.5,
+        x: diffX,
+        y: diffY,
+    });
+}
+
+// Додаємо слухача події на рух курсора
+document.addEventListener("mousemove", onMouseMove);
+const sections = [];
+for (let i = 1; i <= 29; i++) {
+    sections.push(document.querySelector(`#section${i}`));
+}
+
+sections.forEach((section) => {
+    section.addEventListener("mouseover", () => {
+        sections.forEach((otherSection) => {
+            if (otherSection === section) {
+                // otherSection.style.autoalpha = 1;
+                gsap.to(otherSection, { autoAlpha: 1 })
+
+            } else {
+                // otherSection.style.autoalpha = 0;
+                gsap.to(otherSection, { autoAlpha: 0.4 })
+
+            }
+        });
+    });
+
+    section.addEventListener("mouseout", () => {
+        sections.forEach((otherSection) => {
+            gsap.to(otherSection, { autoAlpha: 1 })
+        });
+    });
+});
 document.querySelectorAll(counterArr.concat(counterArr2)).forEach(elem => {
     let tempLength = (elem.getTotalLength() + 1).toFixed(0);
     gsap.set(elem, { strokeDasharray: tempLength, strokeDashoffset: tempLength });
 });
 
+gsap.set('#priceLine', { y: 350 })
+gsap.set('#priceText', { y: 20 })
+gsap.set('#moneyEur', { y: 20 })
+gsap.set('#line002', { x: 960 })
+gsap.set('#red_line', { opacity: 0 })
+gsap.set('#numberR', { opacity: 0 })
+gsap.set('#money', { scale: 0, transformOrigin: "50% 50%" })
+gsap.set('#profit', { scale: 0, transformOrigin: "50% 50%" })
+gsap.set('#slCircleG', { scale: 0, transformOrigin: "50% 50%" })
+gsap.to('#slCircle', { rotation: 360, duration: 8, repeat: -1, ease: "none", transformOrigin: "50% 50%" })
+
 let animation = gsap.timeline()
-    .to(counterArr2, { duration: 29, strokeDashoffset: 0 })
+    .to(counterArr2[0], { duration: 29, strokeDashoffset: 0 })
+
     .to(counterArr[0], { duration: 0.8, strokeDashoffset: 0 }, "<")
     .to(counterArr[1], { duration: 0.8, strokeDashoffset: 0 }, ">")
     .to(counterArr[2], { duration: 0.8, strokeDashoffset: 0 }, ">")
     .to(counterArr[3], { duration: 0.8, strokeDashoffset: 0 }, ">")
     .to(counterArr[4], { duration: 0.8, strokeDashoffset: 0 }, ">")
     .to(counterArr[5], { duration: 0.8, strokeDashoffset: 0 }, ">")
+    .to('#priceLine', { duration: 3, y: 0 }, "<")
+    .to('#numberR', { opacity: 1 }, "<")
+    .to('#red_line', { opacity: 1 }, "<+2")
+
     .to(counterArr[6], { duration: 0.8, strokeDashoffset: 0 }, ">")
     .to(counterArr[7], { duration: 0.8, strokeDashoffset: 0 }, ">")
+    .to('#priceText', { y: 0 }, "<")
     .to(counterArr[8], { duration: 0.8, strokeDashoffset: 0 }, ">")
+
     .to(counterArr[9], { duration: 0.8, strokeDashoffset: 0 }, ">")
     .to(counterArr[10], { duration: 0.8, strokeDashoffset: 0 }, ">")
     .to(counterArr[11], { duration: 0.8, strokeDashoffset: 0 }, ">")
@@ -44,6 +126,8 @@ let animation = gsap.timeline()
     .to(counterArr[13], { duration: 0.8, strokeDashoffset: 0 }, ">")
     .to(counterArr[14], { duration: 0.8, strokeDashoffset: 0 }, ">")
     .to(counterArr[15], { duration: 0.8, strokeDashoffset: 0 }, ">")
+    .to('#slCircleG', { scale: 1, }, "<")
+
     .to(counterArr[16], { duration: 0.8, strokeDashoffset: 0 }, ">")
     .to(counterArr[17], { duration: 0.8, strokeDashoffset: 0 }, ">")
     .to(counterArr[18], { duration: 0.8, strokeDashoffset: 0 }, ">")
@@ -58,10 +142,17 @@ let animation = gsap.timeline()
     .to(counterArr[27], { duration: 0.8, strokeDashoffset: 0 }, ">")
     .to(counterArr[28], { duration: 0.8, strokeDashoffset: 0 }, ">")
     .to(counterArr[29], { duration: 0.8, strokeDashoffset: 0 }, ">")
+    .to('#line002', { duration: 5, x: 0 }, ">")
+    .to('#profit', { duration: 1, scale: 1.4 }, ">-0.2")
+    .to('#profit', { duration: 1, scale: 1 }, ">")
+    .to('#moneyEur', { y: 0 }, "<")
+    .to('#money', { scale: 1.1 }, ">+0.2")
+    .to('#money', { scale: 1 }, ">")
+
 
 // let animationLine = gsap.timeline({})
 // .to(counterArr2, { strokeDashoffset: 0 });
-let scrollDurSection = animation.duration() * 50;
+let scrollDurSection = animation.duration() * 150;
 
 // Перший трігер для анімації counterArr
 ScrollTrigger.create({
